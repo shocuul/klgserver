420bd1912fd99d9835a2fecbb943614147a64b7d
Shocuul <shocuul@live.com>
2015-07-08 18:03:02 -0500

    dfsa

---
+++ api/controllers/AuthController.js
@@ -0,0 +1,42 @@
+module.exports{
+  authenticate : function(req,res){
+    var email = req.param('email');
+    var password = req.param('password');
+
+    if(!email || !password){
+      return res.json(401,{err:'username and password required'});
+    }
+
+    User.findOneByEmail(email, function(err,user){
+      if(!user){
+        return res.json(401,{err:'invalid username or password'});
+      }
+      User.validPassword(password, user, function(err, valid){
+        if(err){
+          return res.json(403, {err: 'forbidden'});
+        }
+        if(!valid){
+          return res.json(401,{err:'invalid username or password'});
+        }else{
+          res.json({user: user, token: sailsTokenAuth.issueToken({sid: user.id})});
+        }
+      });
+    })
+  },
+  register : function(req,res){
+    //TODO: Do some validation on the input.
+    if(req.body.password !== req.body.confirmPassword){
+      return res.json(401, {err: 'Password doesn\'t match'});
+    }
+
+    User.create({email: req.body.email, password: req.body.password}).exec(function(err, user){
+      if(err){
+        res.json(err.status,{err: err});
+        return;
+      }
+      if(user){
+        res.json({user:user, token: sailsTokenAuth.issueToken({sid:user.id})});
+      }
+    });
+  }
+};
