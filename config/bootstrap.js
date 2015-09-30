448a3ef1d02286af7a1c8de5d0689d6aa4fccef7
dev <dev@localhost.localdomain>
2015-09-30 18:16:08 -0500

    shell test

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -16,7 +16,21 @@ module.exports.bootstrap = function(cb) {
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
   //mkdir('-p','out/release');
-  Utils.createXMLServerConfig({name:'Josep',memory:1048576});
+  require('shelljs/global');
+  //Utils.createXMLServerConfig({name:'Josep',memory:1048576});
+  var version = exec('node --version', {silent:true}).output;
+  sails.log(version);
+  exec('virt-install --connect qemu:///system -n kvm1 -r 2048 --vcpus=2 \
+--disk path=/var/lib/libvirt/images/kvm1-image.img,size=20 \
+-c /isos/ubuntu-min-install.iso --vnc --noautoconsole --os-type linux \
+--accelerate --network=network:default --hvm --vncport=5951',{async:true},function(code,output){
+	sails.log(code);
+	sails.log(output);
+})
+  exec('adduser dev libvirtd',{silent:true},function(code, output){
+  	sails.log.warn(code);
+  	sails.log.warn(output);
+  })
   cb();
 
 };
